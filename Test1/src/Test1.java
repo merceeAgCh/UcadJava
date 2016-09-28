7963d7a2e56eb06c66b9ba5d3abf47520fa816bc
mercedes aguilar <mercedes.aguilar95@hotmail.com>
2016-09-28 12:35:32 -0500

    test1
    28-09

---
+++ Test1/src/Test1.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Usuario on 28/09/2016.
+ */
+public class Test1 {
+    public static void main (String []args){
+        int n=65;
+        System.out.println("Decimal: " + Integer.toString(n));
+        System.out.println("Binario: " + Integer.toBinaryString(n));
+        System.out.println("Octal: " + Integer.toOctalString(n));
+        System.out.println("Hexadecimal: " + Integer.toHexString(n));
+        System.out.println("Termario: " + Integer.toString(n,3));
+        n=Integer.parseInt("CAFE ", 16);
+        System.out.println("Valor de n: " + n);
+    }
+}
